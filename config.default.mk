d8976d6b4180f91a88347a3ba2238cafbc35120a
chrender <chrender>
2012-01-26 12:53:15 +0000

    - Added config.*.mk skeletons.

---
+++ config.default.mk
@@ -0,0 +1,20 @@
+
+CC = gcc
+AR = ar
+override CFLAGS += -Wall -Wextra
+
+prefix = $(DESTDIR)/usr/local
+bindir = $(prefix)/games
+datarootdir = $(prefix)/share
+mandir = $(datarootdir)/man
+localedir = $(datarootdir)/fizmo/locales
+
+
+# -----
+# General settings:
+ENABLE_OPTIMIZATION = 1
+#ENABLE_TRACING = 1
+#ENABLE_GDB_SYMBOLS = 1
+# -----
+
+
---
+++ config.macports.mk
@@ -0,0 +1,19 @@
+
+CC = gcc
+AR = ar
+override CFLAGS += -Wall -Wextra
+
+prefix = $(DESTDIR)/opt/local
+bindir = $(prefix)/bin
+datarootdir = $(prefix)/share
+mandir = $(datarootdir)/man
+localedir = $(datarootdir)/fizmo/locales
+
+
+# -----
+# General settings:
+ENABLE_OPTIMIZATION = 1
+#ENABLE_TRACING = 1
+#ENABLE_GDB_SYMBOLS = 1
+# -----
+
